543cb5f8e257cf6e420d2de1bc2d6a3bd60c9c68
Aik Kiat <millerman734@gmail.com>
2026-09-18 23:15:29 +0800

    Add: combination-target-sum-ii - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-2.py
@@ -0,0 +1,49 @@
+from collections import Counter
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        """
+        Array of integers candidates, containing duplicates. Only able to choose from this list.
+        Return a list of uniqur combinations of candidates, chosen numbers sum to target.
+        Each element chosen at once only.
+
+        2 2 4 6 1 5 9
+        0 1 2 3 5 6 7
+
+        [2]
+        |
+        [2,2]  [2,4]
+        |
+        [2,2,4]   [2,4,6]
+        |
+        correct    cannot
+        |go back to [2,2] --> pick [2,4] --> results in right branch --> go back again (pop) to [2] --> pick 6 --> [2,6] --> correct! --> then pop again --> [2] --> [2,1] --> havent reached target or more than target (still < target) --> [2,1,5] correct! (again, add to global list) --> pop again --> this time pop just one level (always return back up to the precedent level) --> [2,1] --> add 9 --> [2,1,9] --> cannot --> cus for loop is done, pop() --> [2,1] --> pop() --> [2,5]...and the sequence continues.
+        """
+
+        candidates.sort()
+
+        def recurse_and_return(index, subset, total):
+            if total == target:
+                result.append(subset[:])
+                return
+
+            elif total > target or index >= len(candidates): #cannot recurse further anymore, as we cannot create a new for loop iteration starting from this index to elements beyond it. Since we have already run to the end of the list
+                return
+
+
+            for i in range(index, len(candidates)):
+                if i > index and candidates[i] == candidates[i-1]:
+                    continue #we must go past the index first, and then once we are past it, we are exploring all the possible branches under this chosen, given root which is represented by the element at index.
+                
+                subset.append(candidates[i])
+                recurse_and_return(i+1, subset, total+candidates[i])
+                if len(subset) > 0:
+                    subset.pop(-1)
+                
+
+        
+        result = []
+        recurse_and_return(0,[],0)
+
+        return result
+        

bd4b824f65b648a3ee5923eaab8bd8ff5e41dd42
Aik Kiat <millerman734@gmail.com>
2026-09-19 22:18:34 +0800

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,97 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        """
+        We need to create a backtracking route, where every choice is a palindrome. 
+        aab
+        |
+        a               aa           aab
+        |                |
+        a      ab        b
+        |                ^correct
+        b --> correct
+        Hence, [a,a,b] and [aa,b]
+
+        We must check whether a given partition is a valid palindrome first, before we accept that partition and recurse down.
+        """
+
+        def check_valid(s):
+            if len(s) % 2 != 0:
+                middle = len(s) // 2
+                for i in range(len(s) - middle):
+                    if s[middle-i] != s[middle+i]:
+                        return False
+                return True
+
+            else:
+                middleLeft = len(s) // 2 - 1
+                middleRight = len(s)// 2
+                for i in range(len(s) - middleRight):
+                    if s[middleLeft-i] != s[middleRight+i]:
+                        return False
+
+                return True
+
+
+
+        """
+        aab
+        How to recurse:
+        -> a, --> then add a? --> aa --> then add b? --> aab
+        -> At each point, add the next following character and that is considered another branch
+        -> after substring is for exp a --> whats left is this index (where a is) --> onwards --> ab only --> therefore --> start a for loop iteration again starting at this index position 
+        --> add a? --> a --> new branch --> add b? --> ab, new branch --> if index reaches end, return... --> then pop last character, or return to state where character was not added, and then continue outer for loop iteration to next branch flow.
+        """
+        
+
+        s_list = list(s)
+
+        result = []
+
+        def recurse_and_return(index, partition_set):
+
+            if index == len(s) and len(partition_set) > 0:
+                result.append(partition_set[:])
+                return
+            
+            for i in range(index,len(s_list)):
+
+                substr = s[index:i+1]
+
+                if check_valid(substr):
+                    
+                    partition_set.append(substr)
+                    
+                    recurse_and_return(i+1, partition_set)
+                    
+                    if len(partition_set) > 0:
+                        partition_set.pop()
+
+
+        recurse_and_return(0, [])
+
+        return result
+
+
+        """
+        aab
+
+        f(0,...) --> f(1,a, []) --> ps=[a], f2(2,a, [a]) --> ps=[a,a], f3(3,b,[a,a,b])
+                                                                       f3(3,ab,[])
+
+        """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        

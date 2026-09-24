44fd7c3857e8982db2c008b28d2953823637bcaa
Aik Kiat <millerman734@gmail.com>
2026-09-24 14:31:35 +0800

    Add: redundant-connection - submission-5

---
+++ Data Structures & Algorithms/redundant-connection/submission-5.py
@@ -0,0 +1,94 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        """
+        > construct adjacency list first
+        > perform DFS, and if we detect a cycle:
+        1 -> 2 -> 3 -> 4
+        now edge added from 4 to 1 --> 4-> 1 --> removing edge 4,1 is correct
+
+        1 -> 2 -> 3
+        edge from 3->1 --> also remove this edge and we are correct.
+        > try to find a cycle. --> if a cycle is detected given a current node, and one of the neighbours, then we return this edge of [current, neighbour]
+
+        The thing is this is an undirected graph --> so the adjancency list needs to include both source and dest. the cycle detection is done within ONE DFS traversal given a source, and it is particularly the edge from [node -> source] which we have to return
+        """
+
+        #build adjacency list
+
+        adj_list = {}
+
+        for edge in edges:
+            source = edge[0]
+            dest = edge[1]
+
+            if source in adj_list.keys():
+                adj_list[source].append(dest)
+
+            else:
+                adj_list[source] = [dest]
+
+            if dest in adj_list.keys():
+                adj_list[dest].append(source)
+
+            else:
+                adj_list[dest] = [source]
+
+
+        def dfs(node, path, previous):
+
+            path.add(node)
+
+            neighbours = adj_list[node]
+            for n in neighbours:
+                if n in path:
+                    #now we must check if this edge was explicitly mentioned, otherwise we are just going backwards along the same edge
+                    if n == previous:
+                        continue
+                    elif [node, n] not in edges:
+                        continue
+                    else:
+                        return [node,n] if [node,n] in edges else [n,node]
+                        
+                
+                result = dfs(n, path, node)
+                
+                if len(result) > 0:
+                    return result
+
+            path.remove(node)
+
+            return []
+
+        
+        final = []
+        id_ = 0
+
+        #"If there are multiple answers, return the edge that appears last in the input"
+        #So, we need to return the edge that was last to be put, and since the DFS results are based on adjacency list and not exactly ordered, we need to only keep track and return the last possible edge to remove.
+        for source in adj_list.keys():
+            result = dfs(source, set([]), None)
+            if len(result) > 0:
+                if id_ < edges.index(result, 0, len(edges)):
+                    final = result
+                    id_ = edges.index(result, 0, len(edges))
+
+        return final
+
+        """
+        1 -> 2, 3
+        3 -> 1, 4
+        2 -> 1, 4
+        4 -> 3
+        """
+
+
+
+        
+
+
+            
+
+
+
+
+        
